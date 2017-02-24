db707b85459d6732ff16ce15b12605532579f300
sakarat27 <sakarat.27@gmail.com>
2017-02-24 14:32:48 +0700

    add detail

---
+++ detail.php
@@ -0,0 +1,53 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<div>
+    <table class="table">
+        <tr>
+            <th colspan="2">
+                ค่าที่จอดรถ
+            </th>
+        </tr>
+        <tr>
+            <td>หมายเลขทะเบียนรถ</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>หมวดจังหวัด</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>วันเวลาที่เข้า</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>วันเวลาที่ออก</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>จำนวนชั่วโมงที่จอด</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>จำนวนเงินค่าจอด</td>
+            <td></td>
+        </tr>
+        <tr align="center">
+            <td colspan="2">รับเงิน</td>
+        </tr>
+        <tr align="center">
+            <td colspan="2"><input type="text" name="pay"></td>
+        </tr>
+        <tr align="center">
+            <td colspan="2"><botton type="submit">คำนวณเงินทอน</botton></td>
+        </tr>
+    </table>
+</div>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>
+</script>
