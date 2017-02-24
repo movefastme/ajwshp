1197822f3aa0413796366b96940bd3b020b2238b
sakarat27 <sakarat.27@gmail.com>
2017-02-24 15:49:20 +0700

    get parking detail 30%

--- detail.php
+++ detail.php
@@ -18,79 +18,96 @@
         margin-top: 20px;
     }
 </style>
-<div class="col-md-6 col-sm-offset-3">
-    <form class="form-horizontal">
-        <div class="form-group">
-            <label class="col-sm-3 control-label">หมายเลขทะเบียนรถ</label>
-            <div class="col-sm-9">
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-sm-3 control-label">หมวดจังหวัด</label>
-            <div class="col-sm-9">
-            </div>
-        </div>
-        <div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">วันเวลาที่เข้า</label>
-                <div class="col-sm-9">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">วันเวลาที่ออก</label>
-                <div class="col-sm-9">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">รับเงิน</label>
-                <div class="col-sm-9">
-                    <input class="form-control" name="pay" type="text">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-10">
-                    <button type="submit" class="btn btn-default">คำนวณเงินทอน</button>
-                </div>
-            </div>
-        </div>
-        <div id="result" style="display:none">
-            <div class="form-group">
-                <label class="col-sm-3 control-label">จำนวนชั่วโมงที่จอด</label>
-                <div class="col-sm-9">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">จำนวนเงินที่รับ</label>
-                <div class="col-sm-9">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label">จำนวนเงินทอน</label>
-                <div class="col-sm-9">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-12 text-center">จำนวนเงินทอนที่ต้องการ</label>
-            </div>
-            <div class="form-group">
-                <div class="">500.00 บาท จำนวน <span id="bank500"></span> ฉบับ</div>
-            </div>
-            <div class="form-group">
-                <div class="">100.00 บาท จำนวน <span id="bank100"></span> ฉบับ</div>
-            </div>
-            <div class="form-group">
-                <div class="">50.00 บาท จำนวน <span id="bank50"></span> ฉบับ</div>
-            </div>
-            <div class="form-group">
-                <div class="">20.00 บาท จำนวน <span id="bank20"></span> ฉบับ</div>
-            </div>
-            <div class="form-group">
-                <div class="">10.00 บาท จำนวน <span id="bank10"></span> ฉบับ</div>
-            </div>
-        </div>
-    </form>
 
-</div>
+<?php
+require_once ('Parking.php');
+
+$parking = new Parking();
+$res = $parking->get_detail_parking(isset($_GET['parking_id']) ? $_GET['parking_id'] : 0);
+
+if (!$res) {
+    echo "CAR NOT FOUND";
+} else {
+    ?>
+
+    <div class="col-md-6 col-sm-offset-3">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label class="col-sm-3 control-label">หมายเลขทะเบียนรถ</label>
+                <div class="col-sm-9">
+                    <?= $res['car_id'] ?>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">หมวดจังหวัด</label>
+                <div class="col-sm-9">
+                    <?= $res['province'] ?>
+                </div>
+            </div>
+            <div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">วันเวลาที่เข้า</label>
+                    <div class="col-sm-9">
+                        <?=$res['check_in']?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">วันเวลาที่ออก</label>
+                    <div class="col-sm-9">
+                        <?=$res['check_out']?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">รับเงิน</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" name="pay" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-3 col-sm-10">
+                        <button type="submit" class="btn btn-default">คำนวณเงินทอน</button>
+                    </div>
+                </div>
+            </div>
+            <div id="result" style="display:none">
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">จำนวนชั่วโมงที่จอด</label>
+                    <div class="col-sm-9">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">จำนวนเงินที่รับ</label>
+                    <div class="col-sm-9">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">จำนวนเงินทอน</label>
+                    <div class="col-sm-9">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-12 text-center">จำนวนเงินทอนที่ต้องการ</label>
+                </div>
+                <div class="form-group">
+                    <div class="">500.00 บาท จำนวน <span id="bank500"></span> ฉบับ</div>
+                </div>
+                <div class="form-group">
+                    <div class="">100.00 บาท จำนวน <span id="bank100"></span> ฉบับ</div>
+                </div>
+                <div class="form-group">
+                    <div class="">50.00 บาท จำนวน <span id="bank50"></span> ฉบับ</div>
+                </div>
+                <div class="form-group">
+                    <div class="">20.00 บาท จำนวน <span id="bank20"></span> ฉบับ</div>
+                </div>
+                <div class="form-group">
+                    <div class="">10.00 บาท จำนวน <span id="bank10"></span> ฉบับ</div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+<?php } ?>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
